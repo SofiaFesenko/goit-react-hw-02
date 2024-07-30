6fb6f9e96f9e856f0f418d88e3637613c947617f
Sofiia Fesenko <sofia.fesen@gmail.com>
2024-07-30 14:04:08 +0300

    ewd

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,63 @@ import Feedback from './components/Feedback'
 import Notification from './components/Notification'
 
 
+// function App() {
+//   const [opinion, setOpinion] = useState(() => {
+
+//     if (window.localStorage.getItem("opinion") !== null) {
+//       return JSON.parse(window.localStorage.getItem("opinion"));
+//     }
+
+//   })
+
+//   useEffect(() => {
+//     window.localStorage.setItem("opinion", JSON.stringify(opinion))
+//   }, [opinion]);
+
+//   const totalOpinion = opinion.good + opinion.neutral + opinion.bad
+
+//   const totalOpinionPercent = Math.round((opinion.good / totalOpinion) * 100)
+
+
+//   const updateFeedback = feedbackType => {
+//     if (feedbackType == "reset") {
+//       setOpinion({ good: 0, neutral: 0, bad: 0 })
+//     }
+//     else {
+//       setOpinion({...opinion, [feedbackType]: opinion[feedbackType] + 1})
+//     }
+//   }
+
+
+//   return (
+//     <>
+//       <Description/>
+//       <Options updateFeedback={updateFeedback} totalOPinion={totalOpinion}/>
+      
+//       {totalOpinion > 0 ? <Feedback good={opinion.good} neutral={opinion.neutral} bad={opinion.bad} total={totalOpinion} positive={totalOpinionPercent} /> : (<Notification/>)}
+//     </>
+//   )
+// }
+
+
 function App() {
-  const [opinion, setOpinion] = useState(() => {
 
-    if (window.localStorage.getItem("opinion") !== null) {
-      return JSON.parse(window.localStorage.getItem("opinion"));
+  const [opinion, setOpinion] = useState(
+    JSON.parse(window.localStorage.getItem("opinion")) || {
+      good: 0,
+      neutral: 0,
+      bad: 0,
     }
+  );
 
-  })
+
+  // const [opinion, setOpinion] = useState(() => {
+
+  //   if (window.localStorage.getItem("opinion") !== null) {
+  //     return JSON.parse(window.localStorage.getItem("opinion"));
+  //   }
+
+  // })
 
   useEffect(() => {
     window.localStorage.setItem("opinion", JSON.stringify(opinion))
